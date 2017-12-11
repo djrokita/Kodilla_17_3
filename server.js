e3b807db657ba643334dbf47bdaad395ebdc2e42
Krzych <krzych.pierzchala@gmail.com>
2017-12-11 07:27:44 +0100

    Project needs to by verified

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var stringIfyFile;
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/getNote', function(request, response) {
+	console.log('Poszło zapytanie o treść pliku JSON');
+	fs.readFile('./test.json', 'utf8', function(err, data) {
+		if (err) throw err;
+		response.send(data);
+	});
+});
+
+app.post('/updateNote/:note', function(request, response) {
+	console.log('Poszło żądanie o uzupełnienie danych');
+	stringIfyFile = request.params.note;
+	fs.writeFile('./test.json', stringIfyFile, function(err) {
+		if (err) throw err;
+		console.log('file updated');
+	});
+});
+
+var server = app.listen(3000);
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!');
+});
