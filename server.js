408e5399e77ac394cf9aaf948cd0667a8541822d
Krzych <krzych.pierzchala@gmail.com>
2017-12-12 22:15:36 +0100

    Closed stream by end method and enabled updating file content

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var stringIfyFile;
+var myJSON;
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
@@ -17,10 +18,17 @@ app.get('/getNote', function(request, response) {
 
 app.post('/updateNote/:note', function(request, response) {
 	console.log('Poszło żądanie o uzupełnienie danych');
-	stringIfyFile = request.params.note;
-	fs.writeFile('./test.json', stringIfyFile, function(err) {
+	fs.readFile('./test.json', 'utf8', function(err, data) {
 		if (err) throw err;
-		console.log('file updated');
+		stringIfyFile = data;
+		myJSON = JSON.parse(stringIfyFile);
+		myJSON.menu.notes.push(request.params.note);
+		stringIfyFile = JSON.stringify(myJSON);
+		fs.writeFile('./test.json', stringIfyFile, function(err) {
+			if (err) throw err;
+			console.log('file updated');
+			response.end();
+		});
 	});
 });
 
